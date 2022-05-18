8c3b72078c479507df0555a236d1da32bc0bac55
dflopezlo <dflopezlo@unal.edu.co>
2022-05-18 12:55:42 -0500

    se agrega el boton

---
+++ src/Boton_evento.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Boton() {
+    return (
+        <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+            Single toggle
+        </button>
+    )
+}
--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Encabezado } from './Encabezado';
 import { Tabla } from './Tabla';
+import { Boton } from './Boton_evento';
 
 export function Aplicacion(){
     return(
         <React.Fragment>
             <Encabezado/>
             <Tabla/>
+
+            <Tabla/>
+            <Boton/>
         </React.Fragment>
 
     ) 
