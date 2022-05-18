a26d56c3d54fdff70605fae0e5a5b2409febf629
dflopezlo <dflopezlo@unal.edu.co>
2022-05-18 12:27:54 -0500

    Se agrega la tabla al aplicativo

--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Encabezado } from './Encabezado';
+import { Tabla } from './Tabla';
 
 export function Aplicacion(){
     return(
+        <React.Fragment>
+            <Tabla/>
+        </React.Fragment>
 
     ) 
 }
